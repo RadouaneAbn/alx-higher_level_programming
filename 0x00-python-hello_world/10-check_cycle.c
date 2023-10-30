18224c351eda0963e65cf5b2fd9629bff1e324d3
radouane.abounouas <radouaneabn@gmail.com>
2023-10-30 21:06:25 +0100

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = list;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (1);
+		}
+	}
+	return (0);
+}
