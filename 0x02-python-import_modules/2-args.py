bb0b91e95ba464e2affce53da26d9c83e10db311
radouane.abounouas <radouaneabn@gmail.com>
2023-11-02 14:04:48 +0100

    Task 2: fix execute on import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argc = len(argv)
-if argc == 1:
-    print("{} arguments.".format(argc - 1))
-elif argc == 2:
-    print("{} argument:".format(argc - 1))
-else:
-    print("{} arguments:".format(argc - 1))
+    argc = len(argv)
+    if argc == 1:
+        print("{} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
 
-for i in range(1, argc):
-    print("{}: {}".format(i, argv[i]))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
