e6b81ca9082fa128dac258207283062dac282c87
radouane.abounouas <radouaneabn@gmail.com>
2023-11-04 19:20:45 +0100

    Task 11: check 4

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx not in range(0, len(my_list)):
-        return my_list
-    del(my_list[idx])
+    if idx in range(len(my_list)):
+        my_list[idx:idx + 1] = []
     return my_list
