3289b913cb39568fe7d61dee744e36ac7f63d255
radouane.abounouas <radouaneabn@gmail.com>
2023-11-08 23:28:47 +0100

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    value = 0
+    if roman_string is None or roman_string.isalpha() is False:
+        return value
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    while i < len(roman_string):
+        if roman_string[i:i + 2] in ['IV', 'IX']:
+            value += {'IV': 4, 'IX': 9}[roman_string[i:i + 2]]
+            i = i + 1
+        else:
+            value += roman.get(roman_string[i], 0)
+        i = i + 1
+    return value
