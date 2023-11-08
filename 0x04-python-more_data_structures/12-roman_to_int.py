a97c61b015e4837b9c879226fdf384a9e80f634d
radouane.abounouas <radouaneabn@gmail.com>
2023-11-09 00:12:42 +0100

    Task 12: handle more notations

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,10 +5,11 @@ def roman_to_int(roman_string):
         return value
 
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    less = {'IV': 4, 'IX': 9, 'XC': 90, 'CM': 900}
     i = 0
     while i < len(roman_string):
-        if roman_string[i:i + 2] in ['IV', 'IX']:
-            value += {'IV': 4, 'IX': 9}[roman_string[i:i + 2]]
+        if roman_string[i:i + 2] in less:
+            value += less[roman_string[i:i + 2]]
             i = i + 1
         else:
             value += roman.get(roman_string[i], 0)
