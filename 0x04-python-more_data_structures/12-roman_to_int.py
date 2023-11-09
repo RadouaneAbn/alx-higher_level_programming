c7c7f5101f4d80afe73ed4f7636e0b15f35bbf23
radouane.abounouas <radouaneabn@gmail.com>
2023-11-09 02:08:06 +0100

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     value = 0
-    if roman_string is None or roman_string.isalpha() is False:
+    if type(roman_string) is not str or roman_string is None:
         return value
 
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
