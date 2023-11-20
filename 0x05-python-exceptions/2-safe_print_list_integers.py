6eb3778c0c3006e15c3120a3f5d32985b066f7aa
radouane.abounouas <radouaneabn@gmail.com>
2023-11-20 17:53:32 +0100

    use pass in except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
-            count = count + 1
+            count += 1
         except (ValueError, TypeError):
-            i = i + 1
-            continue
-        i = i + 1
-
+            pass
+        i += 1
+    print()
     return count
