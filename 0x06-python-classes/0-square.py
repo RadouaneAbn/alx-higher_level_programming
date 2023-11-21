420209c84a1f1b805c2c0a5e302fe5fb9b2cf64d
radouane.abounouas <radouaneabn@gmail.com>
2023-11-21 16:44:04 +0100

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+Module Name: square_module
+
+This module defines a simple class named Square.
+
+Class:
+    Square: Defines a square.
+
+Attributes:
+    None
+
+Usage:
+    new_square = Square() # creates a new instance named new_square
+"""
+
+
+class Square:
+    """
+    a class that defines a square
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        pass
