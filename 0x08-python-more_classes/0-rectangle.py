91800df3bad01dd4bc7009dca83df3d6c1c1b8d9
radouane.abounouas <radouaneabn@gmail.com>
2023-11-27 19:50:36 +0100

    Task 0: add documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+Rectangle Module
+
+This module defines a simple class named Rectangle.
+
+Class:
+    Rectangle: Defines a rectangle.
+
+Usage:
+    new_rectangle = Rectangle() # creates a new instance named new_rectangle
+"""
+
 
 class Rectangle:
+    """
+    Represents a rectangle.
+    """
     pass
