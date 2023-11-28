2c1eb995bc2d071980b9a8545a77e8923b1e9451
radouane.abounouas <radouaneabn@gmail.com>
2023-11-28 21:02:42 +0100

    test file for max_integer function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+"""Unittest for max_integer([..])"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_value(self):
+        """ Test random order list """
         self.assertEqual(max_integer([65, 54, 98, 51, 0, -65]), 98)
+
+        """ Test max integer at the beginning """
+        self.assertEqual(max_integer([98, 65, 54, 51, 0, -65]), 98)
+
+        """ Test max integer at the end """
+        self.assertEqual(max_integer([65, 54, 51, 0, -65, 98]), 98)
+
+        """ Test negative integers list """
         self.assertEqual(max_integer([-15, -848, -65, -2]), -2)
+
+        """ Test empty list """
         self.assertEqual(max_integer([]), None)
 
     def test_raise(self):
+        """ Test a string """
         with self.assertRaises(TypeError):
             max_integer("Test for str")
+
+        """ Test a list with a string(s) in it """
         with self.assertRaises(TypeError):
             max_integer([10, 15, "School"])
+
+        """ Test a list of lists """
         with self.assertRaises(TypeError):
             max_integer([[1, 2], [3, 4]])
+
+        """ Test one integer """
         with self.assertRaises(TypeError):
             max_integer(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
