45a60c1530bb664ff1f32fca0a86a4ae0d6b0d68
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-17 00:11:38 +0100

    Task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,12 @@
 -- This script converts a database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 
-ALTER TABLE hbtn_0c_0.first_table
+ALTER DATABASE hbtn_0c_0
 	CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+	CONVERT TO CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
