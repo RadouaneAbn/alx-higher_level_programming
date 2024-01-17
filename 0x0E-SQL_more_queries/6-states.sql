a982aac893117835c32598de0616698e4527c78c
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-17 17:28:34 +0100

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- This script creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE IF NOT exists hbtn_0d_usa;
+
+CREATE TABLE IF NOT exists hbtn_0d_usa.states (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	name VARCHAR(256) NOT NULL);
