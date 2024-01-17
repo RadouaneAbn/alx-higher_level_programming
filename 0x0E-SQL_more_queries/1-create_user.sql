4bb33de261cfc104c45894603053c51fec96af50
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-17 15:36:06 +0100

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
 -- This script creates a MySQL server user user_0d_1
 
 CREATE USER IF NOT exists 'user_0d_1'@'localhost'
-	IDENTIFIED WITH auth_socket
-	BY "user_0d_1_pwd";
+	IDENTIFIED BY "user_0d_1_pwd";
 
 GRANT ALL PRIVILEGES
 	ON *.*
