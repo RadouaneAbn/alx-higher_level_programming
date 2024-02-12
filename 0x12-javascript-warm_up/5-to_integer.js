39f9ccb540528e49d01295f251eefc97e0cbe4b7
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-12 18:24:50 +0100

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const passedArgs = process.argv;
+const result = Number(passedArgs[2]);
+
+if (isNaN(result)) {
+  console.log('Not a number');
+} else {
+  console.log(result);
+}
