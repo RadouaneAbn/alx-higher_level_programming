5286a3917f8453da6e3700773ed875376b599dc3
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-16 01:35:30 +0100

    Task 0: the code not wont be executed on import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ Usage:
     ./0-select_states.py <username> <password> <database>
 """
 
-from sys import argv
-import MySQLdb
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
 
-host_u, port_u = "localhost", 3306
-name_u, password_u, database_u = argv[1:4]
+    host_u, port_u = "localhost", 3306
+    name_u, password_u, database_u = argv[1:4]
 
-with MySQLdb.connect(host=host_u, port=port_u, user=name_u,
-                     password=password_u, database=database_u) as db:
-    with db.cursor() as cur:
-        cur.execute(f"SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
+    with MySQLdb.connect(host=host_u, port=port_u, user=name_u,
+                         password=password_u, database=database_u) as db:
+        with db.cursor() as cur:
+            cur.execute("SELECT * FROM states ORDER BY id ASC")
+            query_rows = cur.fetchall()
+            for row in query_rows:
+                print(row)
