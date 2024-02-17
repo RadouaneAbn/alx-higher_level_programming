b6265e5ef7c34451ac015ee6e6f4f8295fe0560a
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-17 07:21:13 +0100

    Task 6: Add documentaion

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" State module """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +8,17 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State class that inherits from Base class
+
+    Attributes:
+        __tablename__ (str): The name of the table.
+        id (int): the unique identifier for the state (primary key).
+        name (str): The name of the state.
+
+    Usage:
+        Define a class that represent a state in a database
+    """
     __tablename__ = 'states'
     id = Column(Integer, nullable=False,
                 primary_key=True, autoincrement=True)
