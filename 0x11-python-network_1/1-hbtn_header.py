383655426b2b8c29c27e062e4b03e7100ea442b6
RadouaneAbn <radouaneabn@gmail.com>
2024-03-01 17:07:58 -0800

    [UPDATE] add if __name__ == "__main__":

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,9 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as r:
-    x_request_id = r.headers.get("X-Request-Id", None)
-    if x_request_id:
-        print(x_request_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as r:
+        x_request_id = r.headers.get("X-Request-Id", None)
+        if x_request_id:
+            print(x_request_id)
