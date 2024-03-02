1fd8ecf59e14867511881a0d2de5f74ec301e980
RadouaneAbn <radouaneabn@gmail.com>
2024-03-01 17:55:43 -0800

    [UPDATE] Fix typo Err

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    res = request.get(url)
+    res = requests.get(url)
     if res.status_code > 400:
         print("Error code: {}".format(res.status_code))
     else:
