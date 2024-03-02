9e91b485fc6b5edb97ffdc27db8a322c629c5ec9
RadouaneAbn <radouaneabn@gmail.com>
2024-03-01 17:02:19 -0800

    [ADD] Task: 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script takes a url and an email, sends a POST request to the url
+with the parameter, and display the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+info = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(info).encode()
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as r:
+    response = r.read().decode('utf8')
+
+print(response)
