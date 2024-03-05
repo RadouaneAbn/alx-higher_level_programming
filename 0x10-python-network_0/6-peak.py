b49361f88f84f23f91824ef6cd526f7487353a4a
RadouaneAbn <radouaneabn.com>
2024-03-05 13:37:31 +0100

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This script contains a fucntion that finds peak in a list of unsorted integers.
+"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    left, right = 0, len(list_of_integers) - 1
+
+    while left <= right:
+        m = left + ((right - left) // 2)
+
+        if m > 0 and list_of_integers[m] < list_of_integers[m - 1]:
+            right = m - 1
+        elif m < len(list_of_integers) - 1 and \
+                list_of_integers[m] < list_of_integers[m + 1]:
+            left = m + 1
+        else:
+            # The peak is found
+            return list_of_integers[m]
