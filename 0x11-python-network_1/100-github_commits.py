d9c9b59b85d34d75262d795bf44b6fe0cc08e3e3
RadouaneAbn <radouaneabn.com>
2024-03-05 12:09:14 +0100

    Task 10: remove sorted() (already sorted) + handle few commits case

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,24 +5,19 @@ This script will get the last 10 commits in rails/rails repository
 import requests
 import sys
 
-user_name, repo = sys.argv[2], sys.argv[1]
 
-# Format the URL
-url = f"https://api.github.com/repos/{user_name}/{repo}/commits"
+if __name__ == "__main__":
+    # Format the URL
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
-# Send the get request
-with requests.get(url) as session:
-    if session.status_code != 200:
-        print("failed to get the repository")
-        sys.exit(1)
-    # Store the commit history
-    commits = session.json()
+    # Send the get request and store the commit history
+    with requests.get(url) as session:
+        commits = session.json()
 
-# Sort the list of commits in reverse
-commits = sorted(commits, key=lambda x: x['commit']['author']['date'],
-                 reverse=True)
-
-# Print the last 10 commits
-for i in range(10):
-    line = commits[i]
-    print("{}: {}".format(line['sha'], line['commit']['author']['name']))
+    try:
+        for i in range(10):
+            line = commits[i]
+            print("{}: {}".format(line['sha'],
+                                  line['commit']['author']['name']))
+    except IndexError:
+        pass
