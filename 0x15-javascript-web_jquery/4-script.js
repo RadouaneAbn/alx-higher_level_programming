1ba4daa809914db5c0185efe337d51aadc9e230b
RadouaneAbn <radouaneabn@gmail.com>
2024-04-03 23:25:43 +0000

    update (use toggleClass)

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,3 +1,1 @@
-$('DIV#toggle_header').on('click', function () {
-  if ($('header').hasClass('green')) { $('header').removeClass('green').addClass('red'); } else { $('header').removeClass('red').addClass('green'); }
-});
+$('DIV#toggle_header').on('click', function () { $('header').toggleClass('green red'); });
